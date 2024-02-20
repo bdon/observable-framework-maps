543626f44d42df59f940c8c4ad38a6d2d934fd3c
bdon <bdon@users.noreply.github.com>
2024-02-20 03:49:34 +0000

    deploy: 948f5a4108899e449c6bd4959e181cc3fda65429

---
+++ _observablehq/stdlib/tex.js
@@ -0,0 +1,1 @@
+import o from"https://cdn.jsdelivr.net/npm/katex@0.16.9/+esm";const r=e();function e(n){return function(){const t=document.createElement("div");return o.render(String.raw.apply(String,arguments),t,{...n,output:"html"}),t.removeChild(t.firstChild)}}r.options=e,r.block=e({displayMode:!0});export{r as default};
---
+++ _observablehq/stdlib/vega-lite.js
@@ -0,0 +1,1 @@
+import*as r from"https://cdn.jsdelivr.net/npm/vega@5.27.0/+esm";import*as t from"https://cdn.jsdelivr.net/npm/vega-lite@5.16.3/+esm";import*as a from"https://cdn.jsdelivr.net/npm/vega-lite-api@5.6.0/+esm";var e=a.register(r,t);export{e as default};
